1cfd1291035363f2f10d6fc79d0b470ba527c481
stevenlu0830 <stevenlu19990830@gmail.com>
2024-10-25 10:05:33 -0700

    Specify codes (write file)

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,1 +1,45 @@
+package persistence;
+
+import java.io.*;
+
+import model.ListOfAsms;
+
+// // Represents a writer that writes JSON representation of assignment lists to file
+public class JsonWriter {
+
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        this.destination = destination;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens the writer; 
+    //          throws FileNotFoundException if destination file cannot be opened for writing
+    public void open() throws FileNotFoundException {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of assignment lists to file
+    public void write(ListOfAsms loa) {
+
+    } 
+
+    // MODIFIES: this
+    // EFFECTS: closes the writer
+    public void close() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        
+    }
+
+}
 
