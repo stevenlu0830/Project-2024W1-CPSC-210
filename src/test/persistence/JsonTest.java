c3ff1d7b82f2eb3fc99a2f746f4be0777f468790
stevenlu0830 <stevenlu19990830@gmail.com>
2024-10-24 19:50:29 -0700

    Design tests (load file)

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,1 +1,21 @@
+package persistence;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import model.AsmType;
+import model.Homework;
+
+public class JsonTest {
+    protected void checkHomework(int hwID, String name, String course, AsmType type, String dueDate, String startTime,
+            String description, String finishTime, long duration, Homework h1) {
+        assertEquals(hwID, h1.getID());
+        assertEquals(name, h1.getName());
+        assertEquals(course, h1.getCourse());
+        assertEquals(type, h1.getType());
+        assertEquals(dueDate, h1.getDueDate());
+        assertEquals(startTime, h1.getStartTime());
+        assertEquals(description, h1.getDescription());
+        assertEquals(finishTime, h1.getFinishTime());
+    }
+}
 
